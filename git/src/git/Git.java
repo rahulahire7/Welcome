19b1e0f7292c775092bfdc9bbb354efa7d85fb81
rahulahire7 <rahul.ahire@gmail.com>
2018-12-30 10:12:42 +0530

    test

---
+++ git/src/git/Git.java
@@ -0,0 +1,12 @@
+package git;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome");
+
+System.out.println("Thank You...");
+	}
+
+}
